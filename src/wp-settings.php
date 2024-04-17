c00345c77924caae93294d9cd42df7fc6c182019
Dennis Snell <dennis.snell@automattic.com>
2024-04-17 20:29:26 +0200

    Load HTML5 Named Character References

--- src/wp-settings.php
+++ src/wp-settings.php
@@ -251,6 +251,7 @@ require ABSPATH . WPINC . '/class-wp-oembed.php';
 require ABSPATH . WPINC . '/class-wp-oembed-controller.php';
 require ABSPATH . WPINC . '/media.php';
 require ABSPATH . WPINC . '/http.php';
+require ABSPATH . WPINC . '/html-api/html5-named-character-entities.php';
 require ABSPATH . WPINC . '/html-api/class-wp-html-attribute-token.php';
 require ABSPATH . WPINC . '/html-api/class-wp-html-span.php';
 require ABSPATH . WPINC . '/html-api/class-wp-html-text-replacement.php';
